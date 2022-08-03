028ea4a9ddf2156fc96969a690929edfb4992c86
nmpereira <nikhilpereira25@gmail.com>
2022-08-03 03:29:08 -0400

    finished the ismatch route

--- server/routes/swipeRoutes.js
+++ server/routes/swipeRoutes.js
@@ -2,6 +2,41 @@ const express = require('express');
 const router = express.Router();
 const { readFromDb, writeToDb, deleteFromDb } = require('../helpers/dbhelpers');
 
+router.post('/ismatch/:userid/:otherid', async (req, res) => {
+	try {
+		const userid = req.params.userid;
+		const otherid = req.params.otherid;
+
+		console.log('ismatch', userid, userid);
+
+		const data = await readFromDb('userid', userid);
+		let response = null;
+		if (data.userswipes[otherid] !== undefined) {
+			if (data.userswipes[otherid] === 'true') {
+				response = true;
+			} else if (data.userswipes[otherid] === 'false') {
+				response = false;
+			} else {
+				res.status(400).send({
+					message: `Something went wrong with user ${userid} and ${otherid} on /ismatch`,
+					data: data,
+					response: response
+				});
+			}
+			const write = await writeToDb({ userid, userswipes: data.userswipes });
+			res.status(200).send({
+				message: `User ${userid} has user ${otherid} with value ${data.userswipes[otherid]}`,
+				data: write,
+				response: response
+			});
+		} else {
+			res.status(404).send({ message: `User ${userid} does not have ${otherid}`, data: data, response: null });
+		}
+	} catch (err) {
+		console.log(err);
+	}
+});
+
 router.post('/:userid/:otherid/:bool', async (req, res) => {
 	try {
 		const bool = req.params.bool;
@@ -24,6 +59,7 @@ router.post('/:userid/:otherid/:bool', async (req, res) => {
 });
 
 // app.get('/nextUser')
+
 // app.get('/isMatch')
 
 module.exports = router;
