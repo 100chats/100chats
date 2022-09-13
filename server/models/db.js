a60da32f839cdf1b958ba5b4ebd8f8f4857fbce0
Jacob Asper <jacobasper191@gmail.com>
2022-09-13 06:51:10 -0700

    remove IIFE in db.js (#16)

--- server/models/db.js
+++ server/models/db.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // mongoose setup
-    (async () => {
-      const conn = await mongoose.connect(process.env.DBSTRING);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    })();
+    const conn = await mongoose.connect(process.env.DBSTRING);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
